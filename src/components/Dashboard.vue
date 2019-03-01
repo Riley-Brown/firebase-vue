c007337c0d03de47749ce4c0eeb3841ca756c87f
Riley-Brown <riley@riley.gg>
2019-03-01 01:08:53 -0500

    Minor style updates

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="dashboard">
     <h3>New Employee</h3>
+
+    <div class="fixed-action-btn">
+      <router-link to="/new" class="btn-floating btn-large red">
+        <i class="fa fa-plus"></i>
+      </router-link>
+    </div>
   </div>
 </template>
 
