e8cceeec5a540184cc863fc1593268eec80b12a3
Riley-Brown <riley@riley.gg>
2019-03-01 00:37:22 -0500

    Created components

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="dashboard">
+    <h3>New Employee</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "dashboard",
+  data() {
+    return {};
+  }
+};
+</script>
---
+++ src/components/NewEmployee.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="new-employee">
+    <h3>Dashboard</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "new-employee",
+  data() {
+    return {};
+  }
+};
+</script>
