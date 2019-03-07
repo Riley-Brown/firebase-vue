9abdaf3770f0d06c5e641aa3c21f7d7ca5857c43
Riley-Brown <riley@riley.gg>
2019-03-07 00:13:52 -0500

    Fixed error loading the wrong component for EditEmployee

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import Router from 'vue-router';
 import Dashboard from '@/components/Dashboard';
 import NewEmployee from '@/components/NewEmployee';
 import ViewEmployee from '@/components/ViewEmployee';
-import EditEmployee from '@/components/Dashboard';
+import EditEmployee from '@/components/EditEmployee';
 
 Vue.use(Router);
 
