658fe35eb111e726fc4e79c4de161644a180d922
Riley-Brown <riley@riley.gg>
2019-03-06 23:32:42 -0500

    Created form component for creating new employee

--- src/components/NewEmployee.vue
+++ src/components/NewEmployee.vue
@@ -1,6 +1,36 @@
 <template>
   <div id="new-employee">
     <h3>Dashboard</h3>
+    <div class="row">
+      <form @submit.prevent="saveEmployee" class="col s12">
+        <div class="row">
+          <div class="input-field col s12">
+            <input type="text" v-model="employee_id" required>
+            <label>Employee ID#</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s12">
+            <input type="text" v-model="name" required>
+            <label>Name</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s12">
+            <input type="text" v-model="dept" required>
+            <label>Department</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s12">
+            <input type="text" v-model="position" required>
+            <label>Position</label>
+          </div>
+        </div>
+        <button type="submit" class="btn">Submit</button>
+        <router-link to="/" class="btn btn grey">Cancel</router-link>
+      </form>
+    </div>
   </div>
 </template>
 
